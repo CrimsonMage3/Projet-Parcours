7cfe7bd4cf45b27c263eee44ad1075456aa1bd65
Louis Dumas <louis.dumas@gmail.com>
2023-09-28 19:45:19 -0400

    Version à tester

--- src/main.ino
+++ src/main.ino
@@ -1,20 +1,14 @@
-/*
-Projet: Le nom du script
-Equipe: Votre numero d'equipe
-Auteurs: Les membres auteurs du script
-Description: Breve description du script
-Date: Derniere date de modification
-*/
-
-/* ****************************************************************************
-Inclure les librairies de functions que vous voulez utiliser
-**************************************************************************** */
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
 
 int portBruitAmbiant = 4;
 int portBruitEntendue = 5;
-int compteur = 0;
+
+int portDetecteurProximVert = 6;
+int portDetecteurProximRouge = 7;
+
+int compteurDroite = 0;
+int compteurGauche = 0;
 
 void setup()
 {
@@ -23,52 +17,98 @@ void setup()
   pinMode(portBruitAmbiant, INPUT);
   pinMode(portBruitEntendue, INPUT);
 
+  pinMode(portDetecteurProximVert, INPUT);
+  pinMode(portDetecteurProximRouge, INPUT);
+
   Serial.begin(9600);
 
-  detectionSifflet();
+  detectionSifflet(); // Attend le son du siflet
 }
-
-/* ****************************************************************************
-Fonctions de boucle infini (loop())
-**************************************************************************** */
-// -> Se fait appeler perpetuellement suite au "setup"
 
 void loop()
 {
 
-  PID();
+  if (digitalRead(portDetecteurProximRouge) == digitalRead(portDetecteurProximVert) == 1) // Si rien devant avance
+  {
+    AVANCER();
+  }
+  else
+  {
+    // Tourner droite
+    if (digitalRead(portDetecteurProximRouge) == digitalRead(portDetecteurProximVert) == 1) // Si après tourner à droite rien devant avance
+    {
+      AVANCER();
+    }
+    else
+    {
+      // Tourner gauche 2 fois / tourner 180 degré
+      if (digitalRead(portDetecteurProximRouge) == digitalRead(portDetecteurProximVert) == 1) // Si apès tourner à gauche deux fois rien devant avance
+      {
+        AVANCER();
+      }
+      else
+      {              // FIN
+        while (true) // Gay baby jail
+        {
+        }
+      }
+    }
+  }
+  int compteur = 0; // Reset compteur
+
   delay(10); // Delais pour décharger le CPU
 }
 
 void AVANCER()
 {
 
-  while (ENCODER_Read(RIGHT) <= 50000)
+  while (compteurDroite <= 50000) // Boucle qui s'arrête selon la valeur du compteur car les encodeur sont reset à chaque utiliation du pid
   {
     MOTOR_SetSpeed(RIGHT, 0.5); // Maitre
 
     MOTOR_SetSpeed(LEFT, (0.5 + PID())); // Esclave}
     delay(500);
   }
-  int compteur = 0; // Reset compteur et encodeurs
-  ENCODER_Reset(RIGHT);
-  ENCODER_Reset(LEFT);
+}
+
+void TOURNERDROITE()
+{
+  while (compteurDroite <= 50000) // Boucle qui s'arrête selon la valeur du compteur car les encodeur sont reset à chaque utiliation du pid
+  {
+    MOTOR_SetSpeed(RIGHT, -0.5); // Maitre
+
+    MOTOR_SetSpeed(LEFT, (0.5 + PID())); // Esclave}
+    delay(500);
+  }
+}
+
+void TOURNER180()
+{
+  while (compteurDroite <= 50000) // Boucle qui s'arrête selon la valeur du compteur car les encodeur sont reset à chaque utiliation du pid
+  {
+    MOTOR_SetSpeed(RIGHT, 0.5); // Maitre
+
+    MOTOR_SetSpeed(LEFT, (-0.5 - PID())); // Esclave}
+    delay(500);
+  }
 }
 
 double PID()
 {
 
-  double k1 = 0;
-  double k2 = 0;
+  float k1, k2 = 0;
   float KP = 0.001;
   float KI = 0.00002;
+  compteurDroite += ENCODER_Read(RIGHT); // Ajoute la valeur au compteur de droite
+  compteurGauche += ENCODER_Read(LEFT);  // Ajoute la valeur au compteur de gauche
 
-  
-  k1 = abs(ENCODER_Read(RIGHT)) - abs(ENCODER_Read(LEFT));
+  k1 = abs(ENCODER_ReadReset(RIGHT)) - abs(ENCODER_ReadReset(LEFT)); // Trouve le k1, différence encodeur sans cycle
+  k2 = abs(compteurDroite) - abs(compteurGauche);                    // Trouve le k2, différence des compteurs
 
-  k1 = k1 * KP;
-  //Serial.write();
-  return k1 * k2;
+  k1 *= KP;
+  k2 *= KI;
+
+  return k1 + k2;
 }
 
 void detectionSifflet()
@@ -80,17 +120,16 @@ void detectionSifflet()
 
     float A4 = analogRead(portBruitAmbiant);
     float A5 = analogRead(portBruitEntendue);
-/*
-    if ((A5 - A4) <= 5000)
-    {
-      i++;
-    }
-    else
-    {
-      i = 0;
-    }
+    /*
+        if ((A5 - A4) <= 5000)
+        {
+          i++;
+        }
+        else
+        {
+          i = 0;
+        }
 
-    delay(100);*/
+        delay(100);*/
   }
-  
 }
